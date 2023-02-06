6795a70721f3f62123683554aceadda67c3dc761
Ethan Smith <EthanKSmith@gmail.com>
2023-02-06 13:24:32 -0700

    Initial Commit

---
+++ AlfredQuotes.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+public class AlfredQuotes {
+    
+    public String basicGreeting() {
+        // You do not need to code here, this is an example method
+        return "Hello, lovely to see you. How are you?";
+    }
+    
+    public String guestGreeting(String name) {
+        // YOUR CODE HERE
+        return String.format("Hello, %s. Lovely to see you.", name);
+    }
+    
+    public String dateAnnouncement() {
+        Date date = new Date();
+        return String.format("It is currently " + date );
+    }
+    
+    public String respondBeforeAlexis(String conversation) {
+        int a = conversation.indexOf("Alexis");
+        int b = conversation.indexOf("Alfred");
+        if(a != -1){
+          return String.format("Right away, sir. She certainly isn't sophisticated enough for that.");
+        }
+        else if (b != -1){
+          return String.format("At your service. As you wish, naturally.");
+        }
+        else{
+          return String.format("Right. And with that I shall retire.");
+        }
+        
+    }
+
+    
+	// NINJA BONUS
+	// See the specs to overload the guessGreeting method
+    // SENSEI BONUS
+    // Write your own AlfredQuote method using any of the String methods you have learned!
+}
+
